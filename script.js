77d46300fdf99c5724fd9ac1a8e1a492bdbd8c2b
Sukriti Kanauji <sukritikanauji@gmail.com>
2025-06-12 23:55:37 +0530

    adding changes-1

--- script.js
+++ script.js
@@ -43,3 +43,32 @@ function loadingAnimation() {
   });
 }
 loadingAnimation();
+document.addEventListener("mousemove", function (dets) {
+  gsap.to("#cursor", {
+    left: dets.x,
+    top: dets.y,
+  });
+});
+// document.querySelector("#child1").addEventListener("mouseenter", function () {
+//   gsap.to("#cursor", {
+//     transform: "translate(-50%,-50%) scale(1)",
+//   });
+// });
+// document.querySelector("#child1").addEventListener("mouseleave", function () {
+//   gsap.to("#cursor", {
+//     transform: "translate(-50%,-50%) scale(0)",
+//   });
+// });
+var a = document.querySelectorAll(".child");
+a.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    gsap.to("#cursor", {
+      transform: "translate(-50%,-50%) scale(1)",
+    });
+  });
+  elem.addEventListener("mouseleave", function () {
+    gsap.to("#cursor", {
+      transform: "translate(-50%,-50%) scale(0)",
+    });
+  });
+});
