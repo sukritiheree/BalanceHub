ddabca8b1f08c25762473c71809f580209f8b734
Sukriti Kanauji <sukritikanauji@gmail.com>
2025-06-12 18:39:37 +0530

    making play button 2

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //if we use % the height/width will be according to the parent
+function videoConAnimation() {
+  const videoCon = document.querySelector("#video-container");
+  const playbtn = document.querySelector("#play");
+  videoCon.addEventListener("mouseenter", function () {
+    gsap.to(playbtn, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
+  videoCon.addEventListener("mouseleave", function () {
+    gsap.to(playbtn, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
+  videoCon.addEventListener("mousemove", function (dets) {
+    gsap.to(playbtn, {
+      left: dets.x - 50,
+      top: dets.y - 50,
+    });
+  });
+}
+videoConAnimation();
