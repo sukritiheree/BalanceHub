9d7f4681af0a33e7db8f1d19078443da4be21b40
Sukriti Kanauji <sukritikanauji@gmail.com>
2025-06-12 19:25:49 +0530

    first page completed

--- script.js
+++ script.js
@@ -16,9 +16,25 @@ function videoConAnimation() {
   });
   videoCon.addEventListener("mousemove", function (dets) {
     gsap.to(playbtn, {
-      left: dets.x - 50,
+      left: dets.x - 40,
       top: dets.y - 50,
     });
   });
 }
 videoConAnimation();
+function loadingAnimation() {
+  gsap.from("#page1 h1", {
+    y: 100,
+    opacity: 0,
+    delay: 0.4,
+    duration: 1,
+    stagger: 0.2,
+  });
+  gsap.from("#page1 #video-container", {
+    scale: 0.9,
+    opacity: 0,
+    delay: 1.3,
+    duration: 0.5,
+  });
+}
+loadingAnimation();
