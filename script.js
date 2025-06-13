bd588d8f9af72f9b0c9f0c746d81a58749681fee
Sukriti Kanauji <sukritikanauji@gmail.com>
2025-06-14 01:12:05 +0530

    navbar animation added

--- script.js
+++ script.js
@@ -1,9 +1,82 @@
 //if we use % the height/width will be according to the parent
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true,
-});
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector("#main"),
+//   smooth: true,
+// });
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
 
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+locomotiveAnimation();
+function navbarAnimation() {
+  gsap.to("#navpart1 svg", {
+    transform: "translateY(-100%)",
+    scrollTrigger: {
+      trigger: "#page1",
+      scroller: "#main",
+      start: "top 0",
+      end: "top -5%",
+      scrub: true,
+    },
+  });
+  gsap.to("#navpart1 img", {
+    transform: "translateY(-100%)",
+    scrollTrigger: {
+      trigger: "#page1",
+      scroller: "#main",
+      start: "top 0",
+      end: "top -5%",
+      scrub: true,
+    },
+  });
+  gsap.to("#navpart2 #links", {
+    transform: "translateY(-100%)",
+    opacity: 0,
+    scrollTrigger: {
+      trigger: "#page1",
+      scroller: "#main",
+      start: "top 0",
+      end: "top -5%",
+      scrub: true,
+    },
+  });
+}
+navbarAnimation();
 function videoConAnimation() {
   const videoCon = document.querySelector("#video-container");
   const playbtn = document.querySelector("#play");
@@ -43,32 +116,26 @@ function loadingAnimation() {
   });
 }
 loadingAnimation();
-document.addEventListener("mousemove", function (dets) {
-  gsap.to("#cursor", {
-    left: dets.x,
-    top: dets.y,
-  });
-});
-// document.querySelector("#child1").addEventListener("mouseenter", function () {
-//   gsap.to("#cursor", {
-//     transform: "translate(-50%,-50%) scale(1)",
-//   });
-// });
-// document.querySelector("#child1").addEventListener("mouseleave", function () {
-//   gsap.to("#cursor", {
-//     transform: "translate(-50%,-50%) scale(0)",
-//   });
-// });
-var a = document.querySelectorAll(".child");
-a.forEach(function (elem) {
-  elem.addEventListener("mouseenter", function () {
+
+function cursorAnimation() {
+  document.addEventListener("mousemove", function (dets) {
     gsap.to("#cursor", {
-      transform: "translate(-50%,-50%) scale(1)",
+      left: dets.x,
+      top: dets.y,
     });
   });
-  elem.addEventListener("mouseleave", function () {
-    gsap.to("#cursor", {
-      transform: "translate(-50%,-50%) scale(0)",
+  var a = document.querySelectorAll(".child");
+  a.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+      gsap.to("#cursor", {
+        transform: "translate(-50%,-50%) scale(1)",
+      });
+    });
+    elem.addEventListener("mouseleave", function () {
+      gsap.to("#cursor", {
+        transform: "translate(-50%,-50%) scale(0)",
+      });
     });
   });
-});
+}
+cursorAnimation();
